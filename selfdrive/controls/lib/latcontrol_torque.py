f891baa2ff16a58ed3399f3c4cf373726c0a66e0
JamesL787 <49109285+JamesL787@users.noreply.github.com>
2026-04-04 16:45:24 -0400

    Update latcontrol_torque.py
    Fix indent

--- selfdrive/controls/lib/latcontrol_torque.py
+++ selfdrive/controls/lib/latcontrol_torque.py
@@ -167,11 +167,11 @@ class LatControlTorque(LatControl):
 
     # unwind bleed
     if abs(setpoint) < abs(measurement):
-    self.pid.i *= 0.5
+      self.pid.i *= 0.5
 
     # wrong-direction reset
     if error * self.pid.i < 0:
-    self.pid.i = 0.0
+      self.pid.i = 0.0
 
     freeze_integrator = steer_limited_by_safety or CS.steeringPressed or CS.vEgo < 5
     output_lataccel = self.pid.update(
